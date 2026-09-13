b9a9ba9556e20d38db7ebdef35689cb78dba64d8
Govind Kumar Jha <jhag92630@gmail.com>
2026-09-13 19:54:27 +0530

    added guest feature

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "../App.css"
 import { Link, useNavigate } from 'react-router-dom';
 
-export default function landing(){
+export default function Landing(){
     const router = useNavigate();
     return(
         <div className='landingContainer'>
